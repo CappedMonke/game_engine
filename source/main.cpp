7a8feb28aca50906f75ff82f1e927ea78b6ca44f
CappedMonke <schuergersilasdev@gmail.com>
2024-12-30 22:15:06 +0100

    use sdl instead of glfw

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,36 @@
+#define SDL_MAIN_USE_CALLBACKS
+
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_video.h>
+
 #include "graphics/render_manager.hpp"
 
 
 Render_manager render_manager;
 
-bool exit_game = false;
 
-
-int main(int argc, char* argv[])
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 {
-	// start up managers
-	render_manager.start_up();
+	SDL_SetAppMetadata("Dawn's Ballad", "0.0.0", "com.dawnsballad.www");
+	SDL_Init(0);
 
+	SDL_CreateWindow("Dawn's Ballad", 100, 100, SDL_WINDOW_FULLSCREEN);
 
-	while (!exit_game)
-	{
-	}
+	return SDL_APP_CONTINUE;
+}
 
-	// shut down managers
-	render_manager.shut_down();
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	return SDL_APP_CONTINUE;
+}
 
-	return 0;
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+	SDL_Quit();
 }
