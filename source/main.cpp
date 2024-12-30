b2eba50356c6f37d04b6349a5fc49b8a6c9f516f
CappedMonke <schuergersilasdev@gmail.com>
2024-12-30 19:23:15 +0100

    include glfw instead of setting up window creation myself

--- source/main.cpp
+++ source/main.cpp
@@ -3,11 +3,18 @@
 
 Render_manager render_manager;
 
+bool exit_game = false;
+
 
 int main(int argc, char* argv[])
 {
 	// start up managers
 	render_manager.start_up();
+
+
+	while (!exit_game)
+	{
+	}
 
 	// shut down managers
 	render_manager.shut_down();
