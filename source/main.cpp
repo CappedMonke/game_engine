70b07b7d50ba3b9376111bfb122eeedc79eaccc5
CappedMonke <schuergersilasdev@gmail.com>
2024-12-31 13:18:43 +0100

    updated cmake lists to not repeat fetching sdl for all configurations

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,53 @@
 #define SDL_MAIN_USE_CALLBACKS
+#define GAME_NAME    "Dawn's Ballad"
+#define GAME_VERSION "0.0.0"
+#define GAME_DOMAIN  "com.dawnsballad.www"
 
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_video.h>
 
 #include "graphics/render_manager.hpp"
 
 
-Render_manager render_manager;
+bool keep_running = true;
+
+Render_manager render_manager = {};
 
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 {
-	SDL_SetAppMetadata("Dawn's Ballad", "0.0.0", "com.dawnsballad.www");
-	SDL_Init(0);
+	SDL_SetAppMetadata(GAME_NAME, GAME_VERSION, GAME_DOMAIN);
 
-	SDL_CreateWindow("Dawn's Ballad", 100, 100, SDL_WINDOW_FULLSCREEN);
+	// Init SDL
+	if (!SDL_InitSubSystem(SDL_INIT_AUDIO | SDL_INIT_VIDEO))
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_InitSubSystem error: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	// Init SDL window
+	SDL_Window* window = SDL_CreateWindow(GAME_NAME, 800, 600, SDL_WINDOW_VULKAN);
+	if (!window)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateWindow error: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+	// Init Vulkan
+
 
 	return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
+	if (!keep_running)
+	{
+		return SDL_APP_SUCCESS;
+	}
+
 	return SDL_APP_CONTINUE;
 }
 
