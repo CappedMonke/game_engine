d236c18012bb1db39e27e84d50c2b4f85691401a
CappedMonke <schuergersilasdev@gmail.com>
2025-01-17 20:45:26 +0100

    Added vulkan boilerplate (swapchain etc.)

--- source/graphics/render_manager.cpp
+++ source/graphics/render_manager.cpp
@@ -3,8 +3,12 @@
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_vulkan.h>
+#include <algorithm>
+#include <limits>
+#include <set>
 
 #include "config/application.hpp"
+#include <string>
 
 
 #ifdef NDEBUG
@@ -15,11 +19,12 @@ const bool enable_validation_layers = true;
 
 
 const std::vector<const char*> validation_layers = {"VK_LAYER_KHRONOS_validation"};
+const std::vector<const char*> device_extensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
 
 bool Render_manager::startup()
 {
-	window = SDL_CreateWindow(GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_VULKAN & SDL_WINDOW_RESIZABLE);
+	window = SDL_CreateWindow(GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_VULKAN);
 	if (!window)
 	{
 		return false;
@@ -29,8 +34,13 @@ bool Render_manager::startup()
 	{
 		return false;
 	}
-
+	create_surface();
 	setup_debug_messenger();
+	pick_physical_device();
+	create_logical_device();
+	create_swapchain();
+	create_image_views();
+	create_graphics_pipeline();
 
 	return true;
 }
@@ -42,8 +52,14 @@ void Render_manager::shutdown()
 		destroy_debug_utils_messenger_ext(vulkan_instance, debug_messenger, nullptr);
 	}
 
+	for (auto image_view : swap_chain_image_views)
+	{
+		vkDestroyImageView(device, image_view, nullptr);
+	}
+	vkDestroySwapchainKHR(device, swap_chain, nullptr);
+	vkDestroyDevice(device, nullptr);
+	SDL_Vulkan_DestroySurface(vulkan_instance, surface, nullptr);
 	vkDestroyInstance(vulkan_instance, nullptr);
-
 	SDL_DestroyWindow(window);
 }
 
@@ -93,6 +109,14 @@ bool Render_manager::create_vulkan_instance()
 	return true;
 }
 
+void Render_manager::create_surface()
+{
+	if (!SDL_Vulkan_CreateSurface(window, vulkan_instance, nullptr, &surface))
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create window surface: %s", SDL_GetError());
+	}
+}
+
 bool Render_manager::check_validation_layer_support()
 {
 	uint32_t layer_count;
@@ -122,6 +146,24 @@ bool Render_manager::check_validation_layer_support()
 	}
 
 	return true;
+}
+
+bool Render_manager::check_device_extension_support(VkPhysicalDevice device)
+{
+	uint32_t extension_count;
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extension_count, nullptr);
+
+	std::vector<VkExtensionProperties> available_extensions(extension_count);
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extension_count, available_extensions.data());
+
+	std::set<std::string> required_extensions(device_extensions.begin(), device_extensions.end());
+
+	for (const auto& extension : available_extensions)
+	{
+		required_extensions.erase(extension.extensionName);
+	}
+
+	return required_extensions.empty();
 }
 
 std::vector<const char*> Render_manager::get_required_extensions()
@@ -196,4 +238,289 @@ void Render_manager::destroy_debug_utils_messenger_ext(VkInstance vulkan_instanc
 	{
 		func(vulkan_instance, debug_messenger, allocator);
 	}
+}
+
+void Render_manager::pick_physical_device()
+{
+	uint32_t device_count = 0;
+	vkEnumeratePhysicalDevices(vulkan_instance, &device_count, nullptr);
+
+	if (device_count == 0)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to find GPUs with Vulkan support.");
+	}
+
+	std::vector<VkPhysicalDevice> devices(device_count);
+	vkEnumeratePhysicalDevices(vulkan_instance, &device_count, devices.data());
+
+	for (const VkPhysicalDevice& device : devices)
+	{
+		if (is_device_suitable(device))
+		{
+			physical_device = device;
+			break;
+		}
+	}
+
+	if (physical_device == VK_NULL_HANDLE)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to find a suitable GPU.");
+	}
+}
+
+bool Render_manager::is_device_suitable(VkPhysicalDevice device)
+{
+	Queue_family_indices indices = find_queue_families(device);
+
+	bool extensions_supported = check_device_extension_support(device);
+
+	bool swap_chain_adequate = false;
+	if (extensions_supported)
+	{
+		Swap_chain_support_details swap_chain_support = query_swap_chain_support(device);
+		swap_chain_adequate                           = !swap_chain_support.formats.empty() && !swap_chain_support.present_modes.empty();
+	}
+
+	return indices.is_complete() && extensions_supported && swap_chain_adequate;
+}
+
+Queue_family_indices Render_manager::find_queue_families(VkPhysicalDevice device)
+{
+	Queue_family_indices indices;
+
+	uint32_t queue_family_count = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, queue_families.data());
+
+	int i = 0;
+	for (const VkQueueFamilyProperties& queue_family : queue_families)
+	{
+		if (queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			indices.graphics_family = i;
+		}
+
+		VkBool32 present_support = false;
+		vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &present_support);
+		if (present_support)
+		{
+			indices.present_family = i;
+		}
+
+		if (indices.is_complete())
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	return indices;
+}
+
+void Render_manager::create_logical_device()
+{
+	Queue_family_indices indices = find_queue_families(physical_device);
+
+	std::vector<VkDeviceQueueCreateInfo> queue_create_infos;
+	std::set<uint32_t>                   unique_queue_families = {indices.graphics_family.value(), indices.present_family.value()};
+
+	float queue_priority = 1.0f;
+	for (uint32_t queue_family : unique_queue_families)
+	{
+		VkDeviceQueueCreateInfo queue_create_info = {};
+		queue_create_info.sType                   = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue_create_info.queueFamilyIndex        = queue_family;
+		queue_create_info.queueCount              = 1;
+		queue_create_info.pQueuePriorities        = &queue_priority;
+		queue_create_infos.push_back(queue_create_info);
+	}
+
+	VkPhysicalDeviceFeatures device_features = {};
+
+	VkDeviceCreateInfo create_info      = {};
+	create_info.sType                   = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	create_info.pQueueCreateInfos       = queue_create_infos.data();
+	create_info.queueCreateInfoCount    = static_cast<uint32_t>(queue_create_infos.size());
+	create_info.pEnabledFeatures        = &device_features;
+	create_info.enabledExtensionCount   = static_cast<uint32_t>(device_extensions.size());
+	create_info.ppEnabledExtensionNames = device_extensions.data();
+	if (enable_validation_layers)
+	{
+		create_info.enabledLayerCount   = static_cast<uint32_t>(validation_layers.size());
+		create_info.ppEnabledLayerNames = validation_layers.data();
+	}
+
+	if (vkCreateDevice(physical_device, &create_info, nullptr, &device) != VK_SUCCESS)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create logical device");
+	}
+
+	vkGetDeviceQueue(device, indices.graphics_family.value(), 0, &graphics_queue);
+	vkGetDeviceQueue(device, indices.present_family.value(), 0, &present_queue);
+}
+
+Swap_chain_support_details Render_manager::query_swap_chain_support(VkPhysicalDevice device)
+{
+	Swap_chain_support_details details;
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+	uint32_t format_count;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &format_count, nullptr);
+
+	if (format_count != 0)
+	{
+		details.formats.resize(format_count);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &format_count, details.formats.data());
+	}
+
+	uint32_t present_mode_count;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &present_mode_count, nullptr);
+
+	if (present_mode_count != 0)
+	{
+		details.present_modes.resize(present_mode_count);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &present_mode_count, details.present_modes.data());
+	}
+
+	return details;
+}
+
+VkSurfaceFormatKHR Render_manager::choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR>& available_formats)
+{
+	for (const auto& available_format : available_formats)
+	{
+		if (available_format.format == VK_FORMAT_B8G8R8A8_SRGB && available_format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+		{
+			return available_format;
+		}
+	}
+
+	return available_formats[0];
+}
+
+VkPresentModeKHR Render_manager::choose_swap_present_mode(const std::vector<VkPresentModeKHR>& available_present_modes)
+{
+	for (const auto& available_present_mode : available_present_modes)
+	{
+		if (available_present_mode == VK_PRESENT_MODE_MAILBOX_KHR)
+		{
+			return available_present_mode;
+		}
+	}
+
+	return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D Render_manager::choose_swap_extent(const VkSurfaceCapabilitiesKHR& capabilities)
+{
+	if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+	{
+		return capabilities.currentExtent;
+	}
+	else
+	{
+		int width;
+		int height;
+		SDL_GetWindowSizeInPixels(window, &width, &height);
+
+		VkExtent2D actual_extent = {static_cast<uint32_t>(width), static_cast<uint32_t>(height)};
+
+		actual_extent.width  = std::clamp(actual_extent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actual_extent.height = std::clamp(actual_extent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actual_extent;
+	}
+}
+
+void Render_manager::create_swapchain()
+{
+	Swap_chain_support_details swap_chain_support = query_swap_chain_support(physical_device);
+
+	VkSurfaceFormatKHR surface_format = choose_swap_surface_format(swap_chain_support.formats);
+	VkPresentModeKHR   present_mode   = choose_swap_present_mode(swap_chain_support.present_modes);
+	VkExtent2D         extent         = choose_swap_extent(swap_chain_support.capabilities);
+
+	uint32_t image_count = swap_chain_support.capabilities.minImageCount + 1;
+	if (swap_chain_support.capabilities.maxImageCount > 0 && image_count > swap_chain_support.capabilities.maxImageCount)
+	{
+		image_count = swap_chain_support.capabilities.maxImageCount;
+	}
+
+	VkSwapchainCreateInfoKHR create_info = {};
+	create_info.sType                    = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	create_info.surface                  = surface;
+	create_info.minImageCount            = image_count;
+	create_info.imageFormat              = surface_format.format;
+	create_info.imageColorSpace          = surface_format.colorSpace;
+	create_info.imageExtent              = extent;
+	create_info.imageArrayLayers         = 1;
+	create_info.imageUsage               = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	Queue_family_indices indices                = find_queue_families(physical_device);
+	uint32_t             queue_family_indices[] = {indices.graphics_family.value(), indices.present_family.value()};
+	if (indices.graphics_family != indices.present_family)
+	{
+		create_info.imageSharingMode      = VK_SHARING_MODE_CONCURRENT;
+		create_info.queueFamilyIndexCount = 2;
+		create_info.pQueueFamilyIndices   = queue_family_indices;
+	}
+	else
+	{
+		create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	}
+
+	create_info.preTransform   = swap_chain_support.capabilities.currentTransform;
+	create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	create_info.presentMode    = present_mode;
+	create_info.clipped        = VK_TRUE;
+	create_info.oldSwapchain   = VK_NULL_HANDLE;
+
+	if (vkCreateSwapchainKHR(device, &create_info, nullptr, &swap_chain))
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create swap chain.");
+	}
+
+	vkGetSwapchainImagesKHR(device, swap_chain, &image_count, nullptr);
+	swap_chain_images.resize(image_count);
+	vkGetSwapchainImagesKHR(device, swap_chain, &image_count, swap_chain_images.data());
+
+	swap_chain_image_format = surface_format.format;
+	swap_chain_extent       = extent;
+}
+
+void Render_manager::create_image_views()
+{
+	swap_chain_image_views.resize(swap_chain_images.size());
+
+	for (size_t i = 0; i < swap_chain_images.size(); i++)
+	{
+		VkImageViewCreateInfo create_info           = {};
+		create_info.sType                           = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		create_info.image                           = swap_chain_images[i];
+		create_info.viewType                        = VK_IMAGE_VIEW_TYPE_2D;
+		create_info.format                          = swap_chain_image_format;
+		create_info.components.r                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.g                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.b                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.a                    = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.subresourceRange.aspectMask     = VK_IMAGE_ASPECT_COLOR_BIT;
+		create_info.subresourceRange.baseMipLevel   = 0;
+		create_info.subresourceRange.levelCount     = 1;
+		create_info.subresourceRange.baseArrayLayer = 0;
+		create_info.subresourceRange.layerCount     = 1;
+
+		if (!vkCreateImageView(device, &create_info, nullptr, &swap_chain_image_views[0]) != VK_SUCCESS)
+		{
+			SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create image view.");
+		}
+	}
+}
+
+void Render_manager::create_graphics_pipeline()
+{
+	
 }
--- source/graphics/render_manager.hpp
+++ source/graphics/render_manager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <vector>
 #include <vulkan/vulkan_core.h>
 
@@ -7,6 +8,24 @@
 class SDL_Window;
 class SDL_Surface;
 
+
+struct Queue_family_indices
+{
+	std::optional<uint32_t> graphics_family;
+	std::optional<uint32_t> present_family;
+
+	bool is_complete()
+	{
+		return graphics_family.has_value() && present_family.has_value();
+	}
+};
+
+struct Swap_chain_support_details
+{
+	VkSurfaceCapabilitiesKHR        capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR>   present_modes;
+};
 
 class Render_manager
 {
@@ -22,9 +41,20 @@ private:
 	VkSurfaceKHR             surface;
 	VkInstance               vulkan_instance;
 	VkDebugUtilsMessengerEXT debug_messenger;
+	VkPhysicalDevice         physical_device = VK_NULL_HANDLE;
+	VkDevice                 device;
+	VkQueue                  graphics_queue;
+	VkQueue                  present_queue;
+	VkSwapchainKHR           swap_chain;
+	std::vector<VkImage>     swap_chain_images;
+	std::vector<VkImageView> swap_chain_image_views;
+	VkFormat                 swap_chain_image_format;
+	VkExtent2D               swap_chain_extent;
 
 	bool create_vulkan_instance();
+	void create_surface();
 	bool check_validation_layer_support();
+	bool check_device_extension_support(VkPhysicalDevice device);
 
 	std::vector<const char*> get_required_extensions();
 
@@ -38,4 +68,18 @@ private:
 	create_debug_utils_messenger_ext(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* create_info, const VkAllocationCallbacks* allocator, VkDebugUtilsMessengerEXT* debug_messenger);
 	static void populate_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT& create_info);
 	static void destroy_debug_utils_messenger_ext(VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger, const VkAllocationCallbacks* allocator);
+
+	void                 pick_physical_device();
+	bool                 is_device_suitable(VkPhysicalDevice device);
+	Queue_family_indices find_queue_families(VkPhysicalDevice device);
+
+	void                       create_logical_device();
+	Swap_chain_support_details query_swap_chain_support(VkPhysicalDevice device);
+
+	VkSurfaceFormatKHR choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR>& available_formats);
+	VkPresentModeKHR   choose_swap_present_mode(const std::vector<VkPresentModeKHR>& available_present_modes);
+	VkExtent2D         choose_swap_extent(const VkSurfaceCapabilitiesKHR& capabilities);
+	void               create_swapchain();
+	void               create_image_views();
+	void               create_graphics_pipeline();
 };
